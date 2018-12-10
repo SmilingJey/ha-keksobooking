0aecfdf7e8ebaefd6625139488f9de0b0d3b1cb4
Artem Evsyakov <evsyakov@gmail.com>
2018-12-10 08:54:25 +0300

    реализованы ajax запросы к серверу

---
+++ js/backend.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function () {
+  var LOAD_CARDS_URL = 'https://js.dump.academy/keksobooking/data';
+  var SAVE_CARDS_URL = 'https://js.dump.academy/keksobooking';
+
+  window.backend = {
+    loadCards: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
+      xhr.open('GET', LOAD_CARDS_URL);
+
+      xhr.send();
+    },
+
+    saveForm: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
+      xhr.open('POST', SAVE_CARDS_URL);
+      xhr.send(data);
+    }
+  };
+
+})();
